906724a7312f051974b6a109188e322bcecb03c8
Xiaoyu Ye <benyxy@codeaurora.org>
2016-12-19 18:38:53 -0800

    ASoC: msm: qdsp6v2: Add range checking in msm_dai_q6_set_channel_map
    Range checking is added to prevent buffer overflow that due to inputs
    can be set by user space.
    
    CRs-Fixed: 1098363
    Change-Id: I057261291806240ee6d7b8106a5e83a7665e013d
    Signed-off-by: Xiaoyu Ye <benyxy@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-dai-q6-v2.c
@@ -801,6 +801,10 @@ static int msm_dai_q6_set_channel_map(struct snd_soc_dai *dai,
 	case SLIMBUS_4_RX:
 		if (!rx_slot)
 			return -EINVAL;
+		if (rx_num > AFE_PORT_MAX_AUDIO_CHAN_CNT) {
+			pr_err("%s: invalid rx num %d\n", __func__, rx_num);
+			return -EINVAL;
+		}
 		for (i = 0; i < rx_num; i++) {
 			dai_data->port_config.slim_sch.shared_ch_mapping[i] =
 			    rx_slot[i];
@@ -822,6 +826,10 @@ static int msm_dai_q6_set_channel_map(struct snd_soc_dai *dai,
 	case SLIMBUS_5_TX:
 		if (!tx_slot)
 			return -EINVAL;
+		if (tx_num > AFE_PORT_MAX_AUDIO_CHAN_CNT) {
+			pr_err("%s: invalid tx num %d\n", __func__, tx_num);
+			return -EINVAL;
+		}
 		for (i = 0; i < tx_num; i++) {
 			dai_data->port_config.slim_sch.shared_ch_mapping[i] =
 			    tx_slot[i];
